e9511d0679943be7bb4fd36ab3c9d2d8e79de885
Akiyoshi Makoto <makoto.akiyosi@gmail.com>
2020-11-21 16:38:49 +0900

    Shebangを汎用的なものに修正

--- felica.py
+++ felica.py
@@ -1,5 +1,4 @@
-#/usr/bin/python3
-# -*- coding: utf8 -*-
+#!/usr/bin/env python
 from const import *
 import nfc
 
--- view_admin_list.py
+++ view_admin_list.py
@@ -1,5 +1,4 @@
-#/usr/bin/python3
-# -*- coding: utf8 -*-
+#!/usr/bin/env python
 from tkinter import *
 from tkinter import ttk
 from const import *
